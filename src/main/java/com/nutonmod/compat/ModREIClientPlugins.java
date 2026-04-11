f666c2b04a82c845107bf0e27fe3f7879a22c03d
nuton <2558048758@qq.com>
2026-04-11 10:51:48 +0800

    添加了两个功能方块的rei和帕秋莉手册

--- src/main/java/com/nutonmod/compat/ModREIClientPlugins.java
+++ src/main/java/com/nutonmod/compat/ModREIClientPlugins.java
@@ -2,28 +2,51 @@ package com.nutonmod.compat;
 
 import com.nutonmod.block.ModBlocks;
 import com.nutonmod.recipe.PolishingMachineRecipe;
+import com.nutonmod.screen.DimensionalTunerScreen;
+import com.nutonmod.screen.EnergyDisintegratorScreen;
 import com.nutonmod.screen.PolishingMachineScreen;
+import com.nutonmod.world.system.EnergyDisintegratorBossRefiningData;
 import me.shedaniel.math.Rectangle;
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
 import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
 import me.shedaniel.rei.api.client.registry.screen.ScreenRegistry;
 import me.shedaniel.rei.api.common.util.EntryStacks;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.recipe.CraftingRecipe;
+import net.minecraft.recipe.RecipeType;
 
 public class ModREIClientPlugins implements REIClientPlugin {
     @Override
     public void registerCategories(CategoryRegistry registry) {
         registry.add(new PolishingMachineCategory());
+        registry.add(new DimensionalTunerCategory());
+        registry.add(new EnergyDisintegratorCategory());
         registry.addWorkstations(PolishingMachineCategory.POLISHING_MACHINE, EntryStacks.of(ModBlocks.POLISHING_MACHINE));
+        registry.addWorkstations(DimensionalTunerCategory.DIMENSIONAL_TUNER, EntryStacks.of(ModBlocks.DIMENSIONAL_TUNER));
+        registry.addWorkstations(EnergyDisintegratorCategory.ENERGY_DISINTEGRATOR, EntryStacks.of(ModBlocks.ENERGY_DISINTEGRATOR));
     }
 
     @Override
     public void registerDisplays(DisplayRegistry registry) {
         registry.registerRecipeFiller(PolishingMachineRecipe.class, PolishingMachineRecipe.Type.INSTANCE, PolishingMachineDisplay::new);
+        for (DimensionalTunerDisplay display : DimensionalTunerDisplay.all()) {
+            registry.add(display);
+        }
+        registry.registerRecipesFiller(CraftingRecipe.class, RecipeType.CRAFTING, EnergyDisintegratorDisplay::fromCraftingRecipe);
+        var resourceManager = MinecraftClient.getInstance() != null ? MinecraftClient.getInstance().getResourceManager() : null;
+        for (var entry : EnergyDisintegratorBossRefiningData.getAllProfiles(resourceManager).entrySet()) {
+            registry.add(EnergyDisintegratorDisplay.fromBossProfile(entry.getKey(), entry.getValue()));
+        }
+        registry.add(EnergyDisintegratorDisplay.essence());
     }
     @Override
     public void registerScreens(ScreenRegistry registry) {
         registry.registerClickArea(screen -> new Rectangle(75,30,20,30), PolishingMachineScreen.class,
                 PolishingMachineCategory.POLISHING_MACHINE);
+        registry.registerClickArea(screen -> new Rectangle(48,45,24,12), DimensionalTunerScreen.class,
+                DimensionalTunerCategory.DIMENSIONAL_TUNER);
+        registry.registerClickArea(screen -> new Rectangle(48,35,24,12), EnergyDisintegratorScreen.class,
+                EnergyDisintegratorCategory.ENERGY_DISINTEGRATOR);
     }
 }
